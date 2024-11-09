35efb8b1fe6b9384ac3f173290e3d03c2f184f4b
Salgadoles <salgabrowndazn@gmail.com>
2024-11-09 19:31:45 -0300

    Creating text for title and currency label.

--- steam.py
+++ steam.py
@@ -3,4 +3,13 @@ import customtkinter
 janela = customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("dark-blue")
 janela = customtkinter.CTk()
+
 janela.geometry("500x500")
+titulo = customtkinter.CTkLabel(janela, text="conversor de moedas")
+selecionador = customtkinter.CTkLabel(janela, text="selecione sua moeda")
+
+titulo.pack(padx=10, pady=10)
+selecionador(padx=10, pady=10)
+
+janela.mainloop()
+
