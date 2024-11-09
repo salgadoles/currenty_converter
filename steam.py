82acf4f03f18b02598816715ecaad870e59f4854
Salgadoles <salgabrowndazn@gmail.com>
2024-11-09 18:22:43 -0300

    Creating init window.

---
+++ steam.py
@@ -0,0 +1,6 @@
+import customtkinter
+
+janela = customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("dark-blue")
+janela = customtkinter.CTk()
+janela.geometry("500x500")
