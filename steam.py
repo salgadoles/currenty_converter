29c50dc1f730c3923eb21c6e84b898b7a706a685
Salgadoles <salgabrowndazn@gmail.com>
2024-11-11 11:20:22 -0300

    Create frame for view names coins.

--- steam.py
+++ steam.py
@@ -12,12 +12,28 @@ textoMoedaFinal = customtkinter.CTkLabel(janela, text="selecione sua moeda")
 selecionadorMoedaInicio = customtkinter.CTkOptionMenu(janela, values=['BTC','USD','BRL','EUR'])
 selecionadorMoedaFinal = customtkinter.CTkOptionMenu(janela, values=['BTC','USD','BRL','EUR'])
 
+def conversor():
+    print('converte poura')
+
+botaoConversor = customtkinter.CTkButton(janela, text= 'conversor', command= conversor)
+
+listaMoedas = customtkinter.CTkScrollableFrame(janela)
+moedasDisponiveis = ['BTC: Bitcon', 'BRL: Real brasileiro', 'USD: Dolar americano', 'EUR: Euro espanhol']
+for moedas in moedasDisponiveis:
+
+    textoMoeda = customtkinter.CTkLabel(listaMoeda, text= moedas)
+
+textoMoeda.pack(padx=10, pady=10)
+
+
 
 titulo.pack(padx=10, pady=10)
 textoMoedaInicio.pack(padx = 10, pady = 10)
 textoMoedaFinal.pack(padx=10, pady=10)
 selecionadorMoedaInicio.pack(padx=10, pady=10)
 selecionadorMoedaFinal.pack(padx=10, pady=10)
+botaoConversor.pack(padx= 10, pady= 10)
+listaMoedas.pack(padx = 10, pady = 10)
 
 janela.mainloop()
 
