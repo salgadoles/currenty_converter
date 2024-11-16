b76454033233aaa644c94acefd432bfaedea5104
Salgadoles <salgabrowndazn@gmail.com>
2024-11-16 14:17:59 -0300

    Creating variables and connecting with elements by id.

---
+++ script.js
@@ -0,0 +1,12 @@
+const amount = document.getElementById("amount"),
+fromCoutry = document.getElementById("fromCountry"),
+toCountry = document.getElementById("toCountry")
+selectedSymbol = document.getElementById("selectedSymbol")
+selectedFromImg = document.getElementById("selectedFromImg")
+selectedToImg = document.getElementById("selectedToImg")
+totate = Document.getElementById(".form-control i"),
+formOutput=document.querySelector(".form-output");
+
+window.addEventListener("load", ()=>{
+    alert(1);
+});
