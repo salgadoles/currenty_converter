2dbd8e5fc7d4a09ab421cb4ad1606d4a1aadbc29
Salgadoles <salgabrowndazn@gmail.com>
2024-11-17 11:31:22 -0300

    connecting code with API.

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 const amount = document.getElementById("amount"),
 fromCoutry = document.getElementById("fromCountry"),
-toCountry = document.getElementById("toCountry")
-selectedSymbol = document.getElementById("selectedSymbol")
-selectedFromImg = document.getElementById("selectedFromImg")
-selectedToImg = document.getElementById("selectedToImg")
-totate = Document.getElementById(".form-control i"),
-formOutput=document.querySelector(".form-output");
+toCountry = document.getElementById("toCountry"),
+selectedSymbol = document.getElementById("selectedSymbol"),
+selectedFromImg = document.getElementById("selectedFromImg"),
+selectedToImg = document.getElementById("selectedToImg"),
+//Rotate = Document.querySelector(".form-control i"),
+formOutput = document.querySelector(".form-output");
 
-window.addEventListener("load", ()=>{
-    alert(1);
+window.addEventListener("load", () => {
+    fetch("https://restcountries.com/v3.1/all")
+    .then((response)=>response.json())
+    .then((data) =>{
+        console.log(data);
+    });
+
 });
